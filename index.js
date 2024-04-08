b51ac3e6ab9c57f2d9bd617e1c84f5d24b532842
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-04-08 23:13:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function functionToBeReceived(){
+  
+};
+
+// TODO:- Take a callback function as an argument.
+function receivesAFunction(functionToBeReceived){
+  // TODO:- Call the callback function.
+  functionToBeReceived();
+};
+
+receivesAFunction()
+
+// TODO:- Take no arguments.
+function returnsANamedFunction(){
+  // TODO:- Return a named function.
+  return function namedFunction(){};
+};
+
+// TODO:- Take no arguments.
+function returnsAnAnonymousFunction(){
+  // TODO:- Return an anonymous function.
+  return function (){};
+};
